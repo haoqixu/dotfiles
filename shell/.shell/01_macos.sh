2f471b2fbf00ee18829087a5c9b1f0fa184f8173
xu0o0 <hqcat6@gmail.com>
2019-05-13 15:49:57 +0800

    shell: macos

--- shell/.shell/01_macos.sh
+++ shell/.shell/01_macos.sh
@@ -5,7 +5,8 @@
 # use GNU coreutils instead of BSD coreutils
 prependpath "/usr/local/opt/coreutils/libexec/gnubin"
 
-# tar excludes macos ._ files
+# disable special creation/extraction of ._* files by tar, etc. on Mac OS X
 # ref: https://superuser.com/questions/259703/get-mac-tar-to-stop-putting-filenames-in-tar-archives
 # ref: https://superuser.com/questions/61185/why-do-i-get-files-like-foo-in-my-tarball-on-os-x
+# ref: https://unix.stackexchange.com/questions/9665/create-tar-archive-of-a-directory-except-for-hidden-files
 [[ $OSTYPE == darwin* ]] && export COPYFILE_DISABLE=1
