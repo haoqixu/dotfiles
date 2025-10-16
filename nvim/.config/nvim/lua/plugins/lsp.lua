2b95460354d556db51b2e6014f3ba81abf9cf457
haoqixu <hq.xu0o0@gmail.com>
2025-10-16 18:25:37 +0800

    nvim: make the lua ls aware of neovim runtime files

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -115,6 +115,10 @@ return {
             diagnostics = {
               globals = { 'vim' },
             },
+            workspace = {
+              -- Make the server aware of neovim runtime files
+              library = vim.api.nvim_get_runtime_file("lua", true),
+            },
           },
         },
       })
